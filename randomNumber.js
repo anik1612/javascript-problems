2effd64210b753682b88f2946edc289a4a35dbd1
Anik Sarker <aniksarker1612@gmail.com>
2020-07-15 03:28:39 +0800

    few math library practice

---
+++ randomNumber.js
@@ -0,0 +1,17 @@
+//floor ceil and round math library 
+var num = 5.125;
+
+var result = Math.floor(num);
+var result2 = Math.ceil(num);
+var result3 = Math.round(num);
+
+console.log(result);
+console.log(result2);
+console.log(result3);
+
+//random number generator 
+
+var dice = Math.random() * 6;
+var randomNumberOutput = Math.round(dice);
+console.log(randomNumberOutput);
+

1def10a081384238be0bac37b80b04c5e76dcbd7
Anik Sarker <aniksarker1612@gmail.com>
2020-07-15 04:08:01 +0800

    find max

--- max.js
+++ max.js
@@ -4,6 +4,7 @@ var max = Math.max(a, b, c);
 
 console.log(max)
 
+//using condition 
 if (a > b && a > c) {
     console.log('max is a');
 } else if (b > a &&  b > c) {
@@ -11,3 +12,13 @@ if (a > b && a > c) {
 } else if (c > a && c > b) {
     console.log('max is c');
 }
+
+//find largest element in an array
+var arr = [1,2,3,4,5]
+var max = arr[0];
+for(var i=1; i<arr.length; i++){
+    if(arr[0]<arr[i]){
+        max = arr[i];
+    }
+}
+console.log(max);
